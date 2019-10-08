02b45bfd26fb437ebdb9a354d720579f17f0676d
Manuel Pineda <azormane@gmail.com>
2019-10-08 08:27:43 -0500

    Project Start
    Splash Screen + Main Activity

---
+++ Randal/SplashScreen.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Randal
+{
+    [Activity(Theme = "@style/Theme.Splash",
+        MainLauncher = true,
+        NoHistory = true)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SplashScreen);
+            Task.Run(() => { Thread.Sleep(3000);
+                RunOnUiThread(() =>
+                {
+                    StartActivity(typeof(MainActivity));
+                });
+            });
+        }
+    }
+}
---
+++ Randal/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "6e2141a0-8aa5-47fb-b331-5ab21474f81d",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "System.Net.Http.HttpClientHandler, System.Net.Http",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
