0a88c32e84b13b0b3391156a946fbd80cb56cb2b
Manuel Pineda <azormane@gmail.com>
2019-10-10 11:40:25 -0500

    Login Screen
    Login Screen whit all the paths.

--- Randal/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ Randal/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "6e2141a0-8aa5-47fb-b331-5ab21474f81d",
+		"XAMARIN_BUILD_ID", "a048384c-310d-4281-9fab-a8a507d346ae",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "System.Net.Http.HttpClientHandler, System.Net.Http",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
